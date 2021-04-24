8fccac7bb308325f60de0a1e2a2e8c7fff83ff43
Ozer SUBASI <ozersubasi@gmail.com>
2021-04-24 18:03:05 +0300

    video-2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-50">Welcome to Next.js!</div>
+  return (
+    <div>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            Merhaba, Ben Özer SUBAŞI. Serverless teknolojileriyle haşır neşir
+            bir Backend Developerım.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae
+            commodi dignissimos eligendi laborum, natus, optio quis quod
+            repellendus sint ut, veniam. Asperiores consectetur cumque debitis
+            eligendi ipsum quis ut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae
+            commodi dignissimos eligendi laborum, natus, optio quis quod
+            repellendus sint ut, veniam. Asperiores consectetur cumque debitis
+            eligendi ipsum quis ut.
+          </p>
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto mt-20">
+        <img src="/papico.JPG" alt="papico" />
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
