3ce4a89648ca5f667fa1b7db834589f47fadea08
Ozer SUBASI <ozersubasi@gmail.com>
2021-04-25 04:00:27 +0300

    fix video-4

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
       <Auth0Provider
           domain="subasio.eu.auth0.com"
-          clientId="DVMp8hCShO4uMadrKX1ma9PHfKlx8T3o"
+          clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
           redirectUri={process.env.NEXT_PUBLIC_URL}
       >
     <div className="antialiased text-gray-700">
