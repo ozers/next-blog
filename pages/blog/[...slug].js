3fdc4a4c1853efaaa425c0f6355725fa6f74b70f
Ozer SUBASI <ozersubasi@gmail.com>
2021-04-25 03:48:50 +0300

    video-4

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,47 +1,68 @@
-import { getMdxNode, getMdxPaths } from 'next-mdx/server'
-import { useHydrate } from 'next-mdx/client'
-import { mdxComponents } from '../../components/mdx-components'
+import { getMdxNode, getMdxPaths } from "next-mdx/server"
+import { useHydrate } from "next-mdx/client"
+import { mdxComponents } from "../../components/mdx-components"
+import { useAuth0 } from "@auth0/auth0-react"
 
-export default function PostPage({post}){
-    const content = useHydrate(post, {
-        components: mdxComponents
-    })
+export default function PostPage({ post }) {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
 
-    return (
-        <div className="site-container">
-            <article>
-                <h1 className="text-4xl font-bold">{post.frontMatter.title}</h1>
-                <p>{post.frontMatter.excerpt}</p>
-                <hr className="my-4"/>
-            </article>
+  const content = useHydrate(post, {
+    components: mdxComponents
+  })
 
-            <div className="prose">
-                {content}
+  return (
+    <div className="site-container">
+      <article>
+        <h1 className="text-4xl font-bold">{post.frontMatter.title}</h1>
+        <p>{post.frontMatter.excerpt}</p>
+        <hr className="my-4" />
+        <div className="prose">{content}</div>
+      </article>
+      <form className="mt-10">
+        <textarea rows="2" className="border border-gray-300 rounded w-full block px-2 py-1" />
+
+        <div className="mt-4">
+          {isAuthenticated ? (
+            <div className="flex items-center space-x-2">
+              <button className="bg-blue-600 px-2 py-1 rounded text-white">Send</button>
+              <img src={user.picture} width={40} className="rounded-full" />
+              <span>{user.name}</span>
+              <button
+                onClick={() =>
+                  logout({ returnTo: process.env.NEXT_PUBLIC_URL + "/blog" })
+                }
+              >
+                x
+              </button>
             </div>
+          ) : (
+            <button className="bg-blue-600 px-2 py-1 rounded text-white" onClick={() => loginWithRedirect()}>Log In</button>
+          )}
         </div>
-
-    )
+      </form>
+    </div>
+  )
 }
 
 export async function getStaticPaths() {
-    return {
-        paths: await getMdxPaths('post'),
-        fallback: false
-    }
+  return {
+    paths: await getMdxPaths("post"),
+    fallback: false
+  }
 }
 
 export async function getStaticProps(context) {
-    const post = await getMdxNode('post', context)
+  const post = await getMdxNode("post", context)
 
-    if (!post) {
-        return {
-            notFound: true
-        }
-    }
-
+  if (!post) {
     return {
-        props: {
-            post
-        }
+      notFound: true
     }
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
 }
